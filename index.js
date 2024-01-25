de98656995a43c0241f0ade96894608173a46feb
AnjasFedo <fedoaf98@gmail.com>
2024-01-25 18:57:18 +0700

    Define Routes and Controller

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT|| 3000
-const host = "0.0.0.0"
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const host = "0.0.0.0";
+const routes = require("./config/routes");
+const path = require("path")
 
-app.get('/', (req, res) => {
-  res.send('Hello u!')
-})
+import expressEjsLayouts from "express-ejs-layouts";
+
+app.use(expressEjsLayouts);
+app.set("views", path.join(__dirname, "app/views"));
+app.set("layout", "layouts/application")
+app.set("view engine", 'ejs')
+
+app.use("/", routes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
