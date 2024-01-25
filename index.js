de3aec6c10fd36cf437d378ad715fb75a3de80f9
AnjasFedo <fedoaf98@gmail.com>
2024-01-25 19:04:38 +0700

    Render a HTML as Response

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const host = "0.0.0.0";
 const routes = require("./config/routes");
 const path = require("path")
 
-import expressEjsLayouts from "express-ejs-layouts";
+const layout = require("express-ejs-layouts");
 
-app.use(expressEjsLayouts);
+app.use(layout);
 app.set("views", path.join(__dirname, "app/views"));
 app.set("layout", "layouts/application")
 app.set("view engine", 'ejs')
