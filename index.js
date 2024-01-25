8816b7231f74665f50e35e0757878301350cce76
AnjasFedo <fedoaf98@gmail.com>
2024-01-25 19:06:39 +0700

    Connected the Static Content

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 const host = "0.0.0.0";
 const routes = require("./config/routes");
-const path = require("path")
+const path = require("path");
 
 const layout = require("express-ejs-layouts");
 
 app.use(layout);
 app.set("views", path.join(__dirname, "app/views"));
-app.set("layout", "layouts/application")
-app.set("view engine", 'ejs')
+app.set("layout", "layouts/application");
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"));
 
 app.use("/", routes);
 
