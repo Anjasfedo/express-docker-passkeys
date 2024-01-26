cb3767a6ba18154abe65360d582e4edeb0b29abf
AnjasFedo <fedoaf98@gmail.com>
2024-01-26 19:06:51 +0700

    Passport.js WebAuthnStrategy

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+const PassportService = require("../app/services/passportsService");
+
+const SessionChallangeStore =
+  require("passport-fido2-webauthn").SessionChallengeStore;
+
+const passportService = new PassportService();
+
+const store = new SessionChallangeStore();
+
+passportService.init(store);
+
 const pages = new (require("../app/controllers/pages"))();
 const auth = new (require("../app/controllers/auth"))();
 const admin = new (require("../app/controllers/admin"))();
